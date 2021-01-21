28b00cefbd37bf544ce3baed8187b612eddc4959
Ariel <arielozary@gmail.com>
2021-01-21 16:31:51 +0200

    fixed console

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import SortingVisualizer from "./Components/SortingVisualizer/SortingVisualizer"
 import "./App.css";
 
 function App() {
-  console.log("hello there!! \nIm happy to see your aree checking console!");
-  console.log("this is my beautiful Sorting Visualizator...");
-  console.log("Currently opnly merge sort available.. stay tuned!");
+  console.log("Hello there!! \nIm happy to see you are checking console!");
+  console.log("This is my beautiful Sorting Visualizator...");
+  console.log("Currently only merge sort available.. stay tuned!");
   return (
     <div className="App">
       <h4>We love sorting</h4>
